a11c48418644b5ce792fea62773cea5def962f9c
Nguyễn Hoài Nam <83760339+foenixice1@users.noreply.github.com>
2021-10-05 17:52:54 +0700

    auth

--- src/main/java/com/trello/configuation/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/trello/configuation/filter/JwtAuthenticationFilter.java
@@ -1,7 +1,12 @@
 package com.trello.configuation.filter;
 
+import com.trello.service.JwtService;
 import com.trello.service.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,12 +16,39 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+    @Autowired
+    private JwtService jwtService;
 
     @Autowired
     private UserServiceImpl userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getJwtFormRequest(request);
+            if (jwt != null && jwtService.validateJwtToken(jwt)) {
+                String userName = jwtService.getUserNameFormJwtToken(jwt);
 
+                UserDetails userDetails = userService.loadUserByUsername(userName);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Can NOT set user authentication -> Message: {}", e);
+        }
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String getJwtFormRequest(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.replace("Bearer ", "");
+        }
+        return null;
     }
 }
