824a2d9280473c00feaed050871e977b21f54782
HaiKAVT <haivtka@gmail.com>
2021-10-05 16:09:20 +0700

    model

---
+++ src/main/java/com/trello/model/MemberWorkspace.java
@@ -0,0 +1,25 @@
+package com.trello.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberWorkspace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    private String role;
+}
+
+
