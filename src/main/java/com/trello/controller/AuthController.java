9746c99caf6d25e92980d6a7e0d1f61c324fb9da
zhuy-nguyenz <graio911@gmail.com>
2021-10-06 11:56:17 +0700

    fixed login

--- src/main/java/com/trello/controller/AuthController.java
+++ src/main/java/com/trello/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.trello.controller;
 
 import com.trello.model.JwtResponse;
+import com.trello.model.Payload.LoginRequest;
 import com.trello.model.User;
 import com.trello.service.JwtService;
 import com.trello.service.user.UserServiceImpl;
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,16 +29,18 @@ public class AuthController {
 
     @Autowired
     private UserServiceImpl userService;
+    @Autowired
+    PasswordEncoder encoder;
 
     @PostMapping("/login")
-    private ResponseEntity<?> login(@RequestBody User user) {
+    private ResponseEntity<?> login(@RequestBody LoginRequest user) {
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String jwt = jwtService.generateTokenLogin(authentication);
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        User currentUser = userService.findByUserName(user.getUserName());
+        User currentUser = userService.findByUserName(user.getUsername());
         return ResponseEntity.ok(new JwtResponse(jwt, currentUser.getId(), userDetails.getUsername(), userDetails.getAuthorities()));
 
     }
