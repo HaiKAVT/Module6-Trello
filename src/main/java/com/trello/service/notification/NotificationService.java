4700a1d6936372b304743562fc5cf5a818e35e38
duky2000 <truongduky2000@gmail.com>
2021-10-11 22:25:28 +0700

    Revert "comment"
    This reverts commit b78124ab

--- src/main/java/com/trello/service/notification/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.trello.service.notification;
-
-import com.trello.model.Notification;
-import com.trello.service.GeneralService;
-
-public interface NotificationService extends GeneralService<Notification> {
-
-}
--- src/main/java/com/trello/service/notification/NotificationServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.trello.service.notification;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class NotificationServiceImpl {
-}
